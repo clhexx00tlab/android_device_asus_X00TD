f97d26cc660a27c20d230f7569f9ddfa8c6f5160
Lunarixus <Nathanb644@hotmail.co.uk>
2021-05-14 19:23:42 +0000

    X00TD: Remove unwanted GApps Packages
    Change-Id: Ie4f08459a6004bb16d7dfec7e7a152a42dc0ffdf
    Signed-off-by: clhex <clhexftw@protonmail.com>

--- device.mk
+++ device.mk
@@ -19,6 +19,11 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     init.device.rc
 
+
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Thermal configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
