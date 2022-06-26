5a7141df034a8f6576c62b369ca9ce16424e2547
clhex <clhexftw@protonmail.com>
2022-06-26 12:52:47 +0530

    X00TD: Bringup ArrowOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_X00TD.mk
-
-COMMON_LUNCH_CHOICES := \
-    aosp_X00TD-userdebug
-
+    $(LOCAL_DIR)/arrow_X00TD.mk
--- arrow_X00TD.mk
+++ arrow_X00TD.mk
@@ -9,14 +9,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common AEX stuff
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common ArrowOS stuff
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_X00TD
+PRODUCT_NAME := arrow_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
